dbc2b99571184bcc35892bead6208e64514ba0a7
Hernán Mercado <hernandmf@gmail.com>
2020-10-13 15:56:44 -0500

    first commit

---
+++ 01 fundamentos/js/alerts.js
@@ -0,0 +1,6 @@
+// let nombre = prompt("cual es tu nombre?");
+
+// console.log(nombre);
+
+const Selection = confirm("are you sure to delete this?");
+console.log(Selection);
---
+++ 01 fundamentos/js/app.js
@@ -0,0 +1,14 @@
+let a = 20,
+  b = 30,
+  c = "hola",
+  d = "spiderman",
+  x = a + b;
+
+// console.log({ a }, { b }, { c }, { d });
+// console.table([a, b, c, d]);
+
+const saludo = c + d;
+
+c = "hola de nuevo";
+
+var miNombre = "Hernando";
