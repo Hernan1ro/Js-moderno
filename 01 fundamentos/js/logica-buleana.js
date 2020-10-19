16358ed350d25919f681885d106c4e227fc7e4c1
Hernán Mercado <hernandmf@gmail.com>
2020-10-19 01:28:06 -0500

    class 9

--- 01 fundamentos/js/logica-buleana.js
+++ 01 fundamentos/js/logica-buleana.js
@@ -34,3 +34,26 @@ console.log(false || false);
 
 console.log(regresaTrue() || regresaFalse());
 console.log("cuatro condiciones:", true || true || true || false);
+
+console.warn("Asignaciones Pro-tip");
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+const a1 = true && "Hola mundo" && 150;
+const a2 = "Hola" && "Mundo" && soyFalso && true;
+const a3 = soyFalso || "ya no soy falso";
+const a4 =
+  soyFalso || soyUndefined || soyNull || "Ya no soy falso de nuevo" || true;
+const a5 =
+  soyFalso ||
+  soyUndefined ||
+  regresaTrue() ||
+  "Ya no soy falso otra vez" ||
+  true;
+console.log({ a1, a2, a3, a4, a5 });
+
+if (true || true || true || false) {
+  console.log("hacer algo");
+} else {
+  console.log("hacer otra cosa");
+}
